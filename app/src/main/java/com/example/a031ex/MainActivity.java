1cae6096e94021143776d8edfe4a624d657249c3
av4195 <av4195@bs.amalnet.k12.il>
2025-11-23 23:29:33 +0200

    3

--- app/src/main/java/com/example/a031ex/MainActivity.java
+++ app/src/main/java/com/example/a031ex/MainActivity.java
@@ -8,11 +8,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Employee> allEmployees = new ArrayList<>();
+
+        allEmployees.add(new HourlyEmployee(1, "ישראל ישראלי", 0, 160, 50));
+        allEmployees.add(new HourlyEmployee(2, "Dana Cohen", 0, 120, 45));
+        allEmployees.add(new Manager(3, "Amit Levi", 15000, "Marketing", 0.15));
+        allEmployees.add(new Salesperson(4, "Lior Ben", 8000, 0.10, 35000));
+        allEmployees.add(new Salesperson(5, "Noa Gil", 7000, 0.07, 22000));
     }
 }
